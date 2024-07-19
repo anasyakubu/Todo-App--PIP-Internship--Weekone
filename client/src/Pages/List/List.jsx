9aa48b2399c3d640250b237c31e5628c3ce353aa
anasyakubu <yakubuanas04@gmail.com>
2024-07-19 22:10:24 +0100

    List Task and Delete Task

--- client/src/Pages/List/List.jsx
+++ client/src/Pages/List/List.jsx
@@ -1,8 +1,32 @@
-// import React from 'react'
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import "./List.css";
 
 const List = () => {
+  const [todo, setTodo] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:9000/api/task/list")
+      .then((result) => {
+        setTodo(result.data.data);
+
+        console.error(result.data.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleDelete = (id) => {
+    axios
+      .delete(`http://localhost:9000/api/task/delete/${id}`)
+      .then((res) => {
+        console.log(res);
+        setTodo(todo.filter((item) => item.id !== id));
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="List p-10">
       <div className="">
@@ -27,16 +51,33 @@ const List = () => {
               </tr>
             </thead>
             <tbody>
-              <tr className="bg-white hover:bg-gray-100">
-                <td className="border border-gray-300 px-4 py-2">
-                  The Sliding Mr.
-                </td>
-                <td className="border border-gray-300 px-4 py-2">
-                  Malcolm Lockyer
-                </td>
-                <td className="border border-gray-300 px-4 py-2">1961</td>
-                <td className="border border-gray-300 px-4 py-2">1961</td>
-              </tr>
+              {todo.map((item) => (
+                <tr className="bg-white hover:bg-gray-100" key={item.id}>
+                  <td className="border border-gray-300 px-4 py-2">
+                    {item.title}
+                  </td>
+                  <td className="border border-gray-300 px-4 py-2">
+                    {item.description}
+                  </td>
+                  <td className="border border-gray-300 px-4 py-2">
+                    {new Date(item.dueDate).toLocaleDateString()}
+                  </td>
+                  <td className="border border-gray-300 px-4 py-2 flex gap-3">
+                    <Link
+                      to={`/update/${item.id}`}
+                      className="p-3 bg-green-500 text-black"
+                    >
+                      Edit
+                    </Link>
+                    <button
+                      className="p-3 bg-green-500 text-black"
+                      onClick={() => handleDelete(item.id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
